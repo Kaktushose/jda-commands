640174eabf18a54b996fbc37f61d3ae37eb18180
stijnb1234 <stijnbannink23@gmail.com>
2021-06-04 15:46:11 +0200

    Changed permission pattern to {foo}

--- src/main/java/com/github/kaktushose/jda/commands/internal/Patterns.java
+++ src/main/java/com/github/kaktushose/jda/commands/internal/Patterns.java
@@ -3,7 +3,7 @@ package com.github.kaktushose.jda.commands.internal;
 import java.util.regex.Pattern;
 
 public class Patterns {
-    private static final Pattern JDAPermissionPattern = Pattern.compile("\\{@@([\\w]+)}");
+    private static final Pattern JDAPermissionPattern = Pattern.compile("\\{([\\w]+)}");
 
     public static Pattern getJDAPermissionPattern() {
         return JDAPermissionPattern;
