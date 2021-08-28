9d5a2251897769128f166df3ad92367e712eaef5
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2021-08-28 16:10:25 +0200

    add parameter unit tests

---
+++ src/test/java/parameters/ParameterTestController.java
@@ -0,0 +1,40 @@
+package parameters;
+
+import com.github.kaktushose.jda.commands.rewrite.annotations.Concat;
+import com.github.kaktushose.jda.commands.rewrite.annotations.Optional;
+import com.github.kaktushose.jda.commands.rewrite.annotations.constraints.Min;
+
+public class ParameterTestController {
+
+    public void varArgs(Object... args) {
+
+    }
+
+    public void primitives(int i) {
+
+    }
+
+    public void optional(@Optional Object argument) {
+
+    }
+
+    public void optionalWithDefault(@Optional("default") Object argument) {
+
+    }
+
+    public void concat(@Concat String argument) {
+
+    }
+
+    public void concat(@Concat Object argument) {
+
+    }
+
+    public void constraint(@Min(10) int i) {
+
+    }
+
+    public void constraintWithMessage(@Min(value = 10, message = "error message") int i) {
+
+    }
+}
