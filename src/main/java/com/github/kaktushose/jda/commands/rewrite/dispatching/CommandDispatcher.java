21480ed020b4c04f3c06b691155d63387ccb69b2
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2021-08-31 18:15:23 +0200

    implement routing

--- src/main/java/com/github/kaktushose/jda/commands/rewrite/dispatching/CommandDispatcher.java
+++ src/main/java/com/github/kaktushose/jda/commands/rewrite/dispatching/CommandDispatcher.java
@@ -1,11 +1,14 @@
 package com.github.kaktushose.jda.commands.rewrite.dispatching;
 
+import com.github.kaktushose.jda.commands.rewrite.dispatching.adapter.ParameterAdapterRegistry;
 import com.github.kaktushose.jda.commands.rewrite.dispatching.filter.Filter;
 import com.github.kaktushose.jda.commands.rewrite.dispatching.filter.FilterRegistry;
 import com.github.kaktushose.jda.commands.rewrite.dispatching.parser.ParserSupervisor;
 import com.github.kaktushose.jda.commands.rewrite.dispatching.parser.impl.MessageParser;
 import com.github.kaktushose.jda.commands.rewrite.dispatching.router.CommandRouter;
 import com.github.kaktushose.jda.commands.rewrite.dispatching.router.Router;
+import com.github.kaktushose.jda.commands.rewrite.dispatching.validation.ValidatorRegistry;
+import com.github.kaktushose.jda.commands.rewrite.reflect.CommandRegistry;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.sharding.ShardManager;
@@ -17,6 +20,9 @@ public class CommandDispatcher {
     private final ParserSupervisor parserSupervisor;
     private final Router router;
     private final FilterRegistry filterRegistry;
+    private final ParameterAdapterRegistry adapterRegistry;
+    private final ValidatorRegistry validatorRegistry;
+    private final CommandRegistry commandRegistry;
 
     public CommandDispatcher(Object jda, boolean isShardManager) {
         this.jda = jda;
@@ -32,10 +38,15 @@ public class CommandDispatcher {
 
         router = new CommandRouter();
         filterRegistry = new FilterRegistry();
+
+        adapterRegistry = new ParameterAdapterRegistry();
+        validatorRegistry = new ValidatorRegistry();
+        commandRegistry = new CommandRegistry(adapterRegistry, validatorRegistry);
+        commandRegistry.index();
     }
 
     public void onEvent(CommandContext context) {
-        router.apply(context);
+        router.findCommands(context, commandRegistry.getCommands());
 
         if (context.isCancelled()) {
             return;
@@ -51,6 +62,22 @@ public class CommandDispatcher {
         // TODO parameter adapting
 
         // TODO command execution
+    }
+
+    public ParserSupervisor getParserSupervisor() {
+        return parserSupervisor;
+    }
+
+    public ParameterAdapterRegistry getAdapterRegistry() {
+        return adapterRegistry;
+    }
+
+    public ValidatorRegistry getValidatorRegistry() {
+        return validatorRegistry;
+    }
+
+    public CommandRegistry getCommandRegistry() {
+        return commandRegistry;
     }
 
     public Object getJda() {
--- src/main/java/com/github/kaktushose/jda/commands/rewrite/dispatching/router/CommandRouter.java
+++ src/main/java/com/github/kaktushose/jda/commands/rewrite/dispatching/router/CommandRouter.java
@@ -1,11 +1,59 @@
 package com.github.kaktushose.jda.commands.rewrite.dispatching.router;
 
 import com.github.kaktushose.jda.commands.rewrite.dispatching.CommandContext;
+import com.github.kaktushose.jda.commands.rewrite.reflect.CommandDefinition;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CommandRouter implements Router {
 
     @Override
-    public void apply(CommandContext context) {
-    }
+    public void findCommands(CommandContext context, Collection<CommandDefinition> commands) {
+        CommandDefinition command = null;
+        String[] input = context.getInput();
 
+        for (int i = input.length - 1; i > -1; i--) {
+
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < i + 1; j++) {
+                sb.append(input[j]).append(" ");
+            }
+            String generatedLabel = sb.toString().trim();
+
+            List<CommandDefinition> possibleCommands = commands.stream().filter(cmd -> cmd.getLabels().stream().anyMatch(label -> {
+                        String[] expectedLabels = label.split(" ");
+                        String[] actualLabels = generatedLabel.split(" ");
+                        if (expectedLabels.length != actualLabels.length) {
+                            return false;
+                        }
+
+                        boolean matches = true;
+                        for (int k = 0; k < expectedLabels.length; k++) {
+                            if (!matches) {
+                                return false;
+                            }
+
+                            // TODO if we have labelIgnoreCase option
+                            if (true) {
+                                matches = expectedLabels[k].toLowerCase().startsWith(actualLabels[k].toLowerCase());
+                            } else {
+                                matches = expectedLabels[k].startsWith(actualLabels[k]);
+                            }
+                        }
+                        return matches;
+                    })
+            ).collect(Collectors.toList());
+
+            if (possibleCommands.size() == 1) {
+                command = possibleCommands.get(0);
+            }
+
+            if (possibleCommands.size() > 1) {
+                context.setCancelled(true);
+            }
+        }
+        context.setCommand(command);
+    }
 }
