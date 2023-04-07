a9cae09c6c525fad1da10158768d04de1105a076
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2023-04-07 16:21:48 +0200

    help generation now checks for all filters

--- src/main/java/com/github/kaktushose/jda/commands/embeds/help/DefaultHelpMessageFactory.java
+++ src/main/java/com/github/kaktushose/jda/commands/embeds/help/DefaultHelpMessageFactory.java
@@ -1,21 +1,24 @@
 package com.github.kaktushose.jda.commands.embeds.help;
 
 import com.github.kaktushose.jda.commands.data.CommandList;
+import com.github.kaktushose.jda.commands.dispatching.GenericContext;
 import com.github.kaktushose.jda.commands.dispatching.commands.CommandContext;
-import com.github.kaktushose.jda.commands.dispatching.filter.impl.PermissionsFilter;
+import com.github.kaktushose.jda.commands.dispatching.filter.Filter;
+import com.github.kaktushose.jda.commands.dispatching.filter.FilterRegistry;
 import com.github.kaktushose.jda.commands.reflect.CommandMetadata;
 import com.github.kaktushose.jda.commands.reflect.ControllerDefinition;
 import com.github.kaktushose.jda.commands.reflect.interactions.CommandDefinition;
 import com.github.kaktushose.jda.commands.settings.GuildSettings;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
-import java.util.stream.Collectors;
 
 /**
  * Implementation of {@link HelpMessageFactory} with default embeds.
@@ -26,12 +29,6 @@ import java.util.stream.Collectors;
  * @since 2.0.0
  */
 public class DefaultHelpMessageFactory implements HelpMessageFactory {
-
-    private final PermissionsFilter filter;
-
-    public DefaultHelpMessageFactory() {
-        filter = new PermissionsFilter();
-    }
 
     /**
      * The pattern that is used to insert prefixes. The default value is {@code {prefix}}.
@@ -61,6 +58,16 @@ public class DefaultHelpMessageFactory implements HelpMessageFactory {
         return new MessageCreateBuilder().setEmbeds(builder.build()).build();
     }
 
+    /**
+     * Gets a {@link Message} to send to get an overview over all available commands. Checks all {@link Filter Filters}
+     * defined in phase
+     * {@link com.github.kaktushose.jda.commands.dispatching.filter.FilterRegistry.FilterPosition#BEFORE_ADAPTING
+     * FilterPosition#BEFORE_ADAPTING} to only display commands a user is allowed to execute.
+     *
+     * @param controllers a {@link Set} of all available {@link ControllerDefinition ControllerDefinitions}
+     * @param context     context the corresponding {@link GenericContext}
+     * @return a {@link Message} to send to get an overview over all available commands
+     */
     @Override
     public MessageCreateData getGenericHelp(@NotNull Set<ControllerDefinition> controllers, @NotNull CommandContext context) {
         GuildSettings settings = context.getSettings();
@@ -74,17 +81,25 @@ public class DefaultHelpMessageFactory implements HelpMessageFactory {
                         PREFIX,
                         settings.getHelpLabel()));
 
-        CommandList filteredList = commandList.stream().filter(command -> {
-            filter.apply(context.setCommand(command));
-            System.out.println(context.isCancelled());
-            return !context.isCancelled();
-        }).collect(Collectors.toCollection(CommandList::new));
+        List<Filter> filters = context.getJdaCommands().getFilterRegistry().getAll(FilterRegistry.FilterPosition.BEFORE_ADAPTING);
+        for (CommandDefinition command : commandList) {
+            context.setCommand(command);
+            for (Filter filter : filters) {
+                filter.apply(context);
+                if (context.isCancelled()) {
+                    commandList.remove(command);
+                    break;
+                }
+            }
+        }
 
-        filteredList.getSortedByCategories().forEach((category, commands) -> {
+        commandList.getSortedByCategories().forEach((category, commands) -> {
             StringBuilder sb = new StringBuilder();
             commands.forEach(command -> sb.append(String.format("`%s`", command.getLabel())).append(", "));
             builder.addField(category, sb.substring(0, sb.length() - 2), false);
         });
+
+        builder.setFooter("This only displays commands that you are allowed to execute");
 
         return new MessageCreateBuilder().setEmbeds(builder.build()).build();
     }
