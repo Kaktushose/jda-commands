ea2f3d60eb845608cf1960d67c070dc34d630ca9
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2023-04-13 13:39:21 +0200

    add api for guild scoped commands

---
+++ src/main/java/com/github/kaktushose/jda/commands/interactions/commands/DefaultGuildScopeProvider.java
@@ -0,0 +1,23 @@
+package com.github.kaktushose.jda.commands.interactions.commands;
+
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * Default implementation of {@link GuildScopeProvider}. <b>This will always return an empty Set!</b>
+ *
+ * @author Kaktushose
+ * @version 4.0.0
+ * @see GuildScopeProvider
+ * @since 4.0.0
+ */
+public class DefaultGuildScopeProvider implements GuildScopeProvider {
+
+    @Override
+    public Set<Long> getGuildsForCommand(CommandData commandData) {
+        return Collections.emptySet();
+    }
+
+}
---
+++ src/main/java/com/github/kaktushose/jda/commands/interactions/commands/GuildScopeProvider.java
@@ -0,0 +1,25 @@
+package com.github.kaktushose.jda.commands.interactions.commands;
+
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+import java.util.Set;
+
+/**
+ * Interface for declaring on which Guilds a guild scoped command should be registered.
+ *
+ * @author Kaktushose
+ * @version 4.0.0
+ * @see DefaultGuildScopeProvider
+ * @since 4.0.0
+ */
+public interface GuildScopeProvider {
+
+    /**
+     * Gets a Set of guild ids the provided command should be registered for.
+     *
+     * @param commandData a copy of the {@link CommandData} to register
+     * @return a Set of guild ids the provided command should be registered for
+     */
+    Set<Long> getGuildsForCommand(CommandData commandData);
+
+}
