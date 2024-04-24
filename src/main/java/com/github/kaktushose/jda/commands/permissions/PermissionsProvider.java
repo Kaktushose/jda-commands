de3da2e5c175b74980f78ae1d9349593d425775f
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2024-04-24 17:54:26 +0200

    remove obsolete methods in PermissionsProvider

--- src/main/java/com/github/kaktushose/jda/commands/permissions/PermissionsProvider.java
+++ src/main/java/com/github/kaktushose/jda/commands/permissions/PermissionsProvider.java
@@ -2,13 +2,9 @@ package com.github.kaktushose.jda.commands.permissions;
 
 import com.github.kaktushose.jda.commands.annotations.interactions.Permissions;
 import com.github.kaktushose.jda.commands.dispatching.interactions.Context;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Interface for performing permission checks.
@@ -39,27 +35,5 @@ public interface PermissionsProvider {
      * @return {@code true} if the user has the permission to execute the command
      */
     boolean hasPermission(@NotNull Member member, @NotNull Context context);
-
-    /**
-     * Gets a {@link List} of user ids that map to the given permission string.
-     *
-     * @param guild      the corresponding guild
-     * @param permission the corresponding string
-     * @return a {@link List} of user ids
-     */
-    default List<Long> getUsersWithPermission(Guild guild, String permission) {
-        return new ArrayList<>();
-    }
-
-    /**
-     * Gets a {@link List} of role ids that map to the given permission string.
-     *
-     * @param guild      the corresponding guild
-     * @param permission the corresponding string
-     * @return a {@link List} of role ids
-     */
-    default List<Long> getRolesWithPermission(Guild guild, String permission) {
-        return new ArrayList<>();
-    }
 
 }
