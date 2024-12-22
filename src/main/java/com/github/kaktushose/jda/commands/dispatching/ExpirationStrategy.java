9b93453c66d729f70c4171cc3e6279c5de8df7f0
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2024-12-22 21:08:51 +0100

    stuff

--- src/main/java/com/github/kaktushose/jda/commands/dispatching/ExpirationStrategy.java
+++ src/main/java/com/github/kaktushose/jda/commands/dispatching/ExpirationStrategy.java
@@ -1,7 +1,8 @@
 package com.github.kaktushose.jda.commands.dispatching;
 
 public sealed interface ExpirationStrategy {
-    Inactivity AFTER_15_MIN = new ExpirationStrategy.Inactivity(15);
+    ExpirationStrategy AFTER_15_MIN = new ExpirationStrategy.Inactivity(15);
+    ExpirationStrategy EXPLICIT = new ExpirationStrategy.Explicit();
 
     record Inactivity(long afterMinutes) implements ExpirationStrategy {}
     record Explicit() implements ExpirationStrategy {}
