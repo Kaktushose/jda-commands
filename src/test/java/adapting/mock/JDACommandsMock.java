5f3fe2a8f3ba2440ade26e86048eb773cdd86dbe
Nick Hensel <nickhensel25@icloud.com>
2025-01-05 18:15:02 +0100

    Fix tests

--- src/test/java/adapting/mock/JDACommandsMock.java
+++ src/test/java/adapting/mock/JDACommandsMock.java
@@ -2,10 +2,10 @@ package adapting.mock;
 
 import com.github.kaktushose.jda.commands.definitions.interactions.InteractionRegistry;
 import com.github.kaktushose.jda.commands.dependency.DependencyInjector;
-import com.github.kaktushose.jda.commands.dispatching.adapter.TypeAdapterRegistry;
+import com.github.kaktushose.jda.commands.dispatching.adapter.internal.TypeAdapters;
 import com.github.kaktushose.jda.commands.dispatching.JDAEventListener;
-import com.github.kaktushose.jda.commands.dispatching.middleware.MiddlewareRegistry;
-import com.github.kaktushose.jda.commands.dispatching.validation.ValidatorRegistry;
+import com.github.kaktushose.jda.commands.dispatching.middleware.internal.Middlewares;
+import com.github.kaktushose.jda.commands.dispatching.validation.internal.Validators;
 import com.github.kaktushose.jda.commands.internal.JDAContext;
 import com.github.kaktushose.jda.commands.internal.register.SlashCommandUpdater;
 
@@ -13,9 +13,9 @@ import com.github.kaktushose.jda.commands.internal.register.SlashCommandUpdater;
 public record JDACommandsMock(
         JDAContext jdaContext,
         JDAEventListener JDAEventListener,
-        MiddlewareRegistry middlewareRegistry,
-        TypeAdapterRegistry adapterRegistry,
-        ValidatorRegistry validatorRegistry,
+        Middlewares middlewareRegistry,
+        TypeAdapters adapterRegistry,
+        Validators validatorRegistry,
         DependencyInjector dependencyInjector,
         InteractionRegistry interactionRegistry,
         SlashCommandUpdater updater) {
