84a73693e0293a693dae30b7e86f45097d084a2c
Nick Hensel <nickhensel25@icloud.com>
2025-01-12 22:10:51 +0100

    Introduce getter for interaction class instances in event

--- jda-commands/src/main/java/com/github/kaktushose/jda/commands/dispatching/events/Event.java
+++ jda-commands/src/main/java/com/github/kaktushose/jda/commands/dispatching/events/Event.java
@@ -90,6 +90,13 @@ public abstract sealed class Event<T extends GenericInteractionCreateEvent> impl
         return runtime.keyValueStore();
     }
 
+    /// Returns an instance of a class annotated with [`Interaction`][com.github.kaktushose.jda.commands.annotations.interactions.Interaction],
+    /// that is bound to the underlying [`Runtime`]({@docRoot}/index.html#runtime-concept-heading).
+    /// @return the interaction class instance
+    @Nullable
+    public <I> I interactionClass(Class<I> interactionClass) {
+        return runtime.interactionClass(interactionClass);
+    }
 
     @Override
     public int getTypeRaw() {
