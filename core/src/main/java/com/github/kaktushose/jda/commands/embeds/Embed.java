d74813ae76f2973050f2c247b177a1b85524231c
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2025-02-25 20:04:17 +0100

    add field removal methods

--- core/src/main/java/com/github/kaktushose/jda/commands/embeds/Embed.java
+++ core/src/main/java/com/github/kaktushose/jda/commands/embeds/Embed.java
@@ -1,6 +1,7 @@
 package com.github.kaktushose.jda.commands.embeds;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.exceptions.ParsingException;
 import net.dv8tion.jda.api.utils.data.DataObject;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
@@ -8,6 +9,7 @@ import net.dv8tion.jda.api.utils.messages.MessageEditData;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,10 +20,56 @@ public class Embed extends EmbedBuilder {
     }
 
     public Embed(@NotNull DataObject object) {
-        super(EmbedBuilder.fromData(object));
+        this(EmbedBuilder.fromData(object));
     }
 
-    // TODO fields API
+    @NotNull
+    public Fields fields() {
+        return new Fields() {
+
+            @NotNull
+            @Override
+            public Fields removeIf(@NotNull Predicate<MessageEmbed.Field> filter) {
+                getFields().removeIf(filter);
+                return this;
+            }
+
+            @Override
+            public @NotNull Fields removeIf(@NotNull String name, Predicate<MessageEmbed.Field> filter) {
+                getFields().removeIf(filter.and(field -> name.equals(field.getName())));
+                return this;
+            }
+        };
+    }
+
+    public interface Fields {
+
+        @NotNull
+        Fields removeIf(@NotNull Predicate<MessageEmbed.Field> filter);
+
+        @NotNull
+        Fields removeIf(@NotNull String name, Predicate<MessageEmbed.Field> filter);
+
+        @NotNull
+        default Fields remove(@NotNull String value) {
+            return removeIf(field -> value.equals(field.getValue()));
+        }
+
+        @NotNull
+        default Fields removeByName(@NotNull String name) {
+            return removeIf(field -> name.equals(field.getName()));
+        }
+
+        @NotNull
+        default Fields remove(@NotNull String name, @NotNull String value) {
+            return removeIf(field -> name.equals(field.getName()) && value.equals(field.getValue()));
+        }
+    }
+
+    public Embed placeholder(Map<String, Object> values) {
+        values.forEach(this::placeholder);
+        return this;
+    }
 
     public Embed placeholder(String placeholder, Object value) {
         String json = build().toData().toString().replaceAll(
@@ -59,11 +107,6 @@ public class Embed extends EmbedBuilder {
             escaped.append(append);
         }
         return escaped.toString();
-    }
-
-    public Embed placeholder(Map<String, Object> values) {
-        values.forEach(this::placeholder);
-        return this;
     }
 
     @NotNull
