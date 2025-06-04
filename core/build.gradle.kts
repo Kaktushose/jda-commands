24bcca8ff5bb1f039e1d5a0937c9ad028fa225fb
Goldmensch <pm@goldmensch.dev>
2025-06-04 18:25:07 +0200

    remove fluava dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -13,7 +13,6 @@ dependencies {
     api(libs.org.slf4j.slf4j.api)
     api(libs.com.google.code.gson.gson)
     api(libs.org.jetbrains.annotations)
-    api(libs.dev.goldmensch.fluava)
 
     compileOnly("jakarta.inject:jakarta.inject-api:2.0.1")
 
