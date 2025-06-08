6c83ca55bcacdb0b2385758b903c59f57847603d
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2025-06-08 16:02:26 +0200

    remove TypeFormat

--- core/src/main/java/com/github/kaktushose/jda/commands/dispatching/adapter/internal/TypeAdapters.java
+++ core/src/main/java/com/github/kaktushose/jda/commands/dispatching/adapter/internal/TypeAdapters.java
@@ -3,7 +3,6 @@ package com.github.kaktushose.jda.commands.dispatching.adapter.internal;
 import com.github.kaktushose.jda.commands.dispatching.adapter.TypeAdapter;
 import io.github.kaktushose.proteus.Proteus;
 import io.github.kaktushose.proteus.mapping.Mapper;
-import io.github.kaktushose.proteus.type.Format;
 import io.github.kaktushose.proteus.type.Type;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.entities.channel.concrete.*;
@@ -11,19 +10,16 @@ import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
 import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Optional;
 import java.util.function.Function;
 
-import static io.github.kaktushose.proteus.mapping.Mapper.bi;
 import static io.github.kaktushose.proteus.mapping.Mapper.uni;
-import static io.github.kaktushose.proteus.mapping.MappingResult.*;
+import static io.github.kaktushose.proteus.mapping.MappingResult.failure;
+import static io.github.kaktushose.proteus.mapping.MappingResult.lossless;
 
 /// Central registry for all type adapters.
 ///
@@ -31,31 +27,20 @@ import static io.github.kaktushose.proteus.mapping.MappingResult.*;
 @ApiStatus.Internal
 public class TypeAdapters {
 
-    private static final Type<String> STRING = Type.of(new TypeFormat(OptionType.STRING), String.class);
-    private static final Type<Long> INTEGER = Type.of(new TypeFormat(OptionType.INTEGER), Long.class);
-    private static final Type<Boolean> BOOLEAN = Type.of(new TypeFormat(OptionType.BOOLEAN), Boolean.class);
-    private static final Type<User> USER = Type.of(new TypeFormat(OptionType.USER), User.class);
-    private static final Type<Member> MEMBER = Type.of(new TypeFormat(OptionType.USER), Member.class);
-    private static final Type<GuildChannelUnion> CHANNEL = Type.of(new TypeFormat(OptionType.CHANNEL), GuildChannelUnion.class);
-    private static final Type<Role> ROLE = Type.of(new TypeFormat(OptionType.ROLE), Role.class);
-    private static final Type<IMentionable> MENTIONABLE = Type.of(new TypeFormat(OptionType.MENTIONABLE), IMentionable.class);
-    private static final Type<Double> NUMBER = Type.of(new TypeFormat(OptionType.NUMBER), Double.class);
-    private static final Type<Message.Attachment> ATTACHMENT = Type.of(new TypeFormat(OptionType.ATTACHMENT), Message.Attachment.class);
+    private static final Type<String> STRING = Type.of(String.class);
+    private static final Type<User> USER = Type.of(User.class);
+    private static final Type<Member> MEMBER = Type.of(Member.class);
+    private static final Type<GuildChannelUnion> CHANNEL = Type.of(GuildChannelUnion.class);
+    private static final Type<Role> ROLE = Type.of(Role.class);
+    private static final Type<IMentionable> MENTIONABLE = Type.of(IMentionable.class);
+    private static final Type<Double> NUMBER = Type.of(Double.class);
     private final Proteus proteus;
 
     /// Constructs a new TypeAdapters.
     @SuppressWarnings("unchecked")
     public TypeAdapters(@NotNull Map<Entry<Type<?>, Type<?>>, TypeAdapter<?, ?>> typeAdapters) {
         proteus = Proteus.global();
-        proteus.from(NUMBER).into(INTEGER, bi(
-                (source, _) -> {
-                    if (source > Long.MAX_VALUE || source < Long.MIN_VALUE) {
-                        return failure("Double value out of bounds");
-                    }
-                    return lossy((source.longValue()));
-                },
-                (source, _) -> lossless(source.doubleValue())
-        ));
+
         proteus.from(NUMBER).into(STRING, uni((source, _) -> lossless(String.valueOf(source))));
 
         proteus.from(MEMBER).into(USER, uni((source, _) -> lossless(source.getUser())));
@@ -65,16 +50,15 @@ public class TypeAdapters {
                 .from(MEMBER, uni((source, _) -> lossless(source)))
                 .from(ROLE, uni((source, _) -> lossless(source)));
 
-        TypeFormat format = new TypeFormat(OptionType.CHANNEL);
         proteus.from(CHANNEL)
-                .into(Type.of(format, GuildChannel.class), uni((source, _) -> lossless(source)))
-                .into(Type.of(format, AudioChannel.class), channel(GuildChannelUnion::asAudioChannel))
-                .into(Type.of(format, GuildMessageChannel.class), channel(GuildChannelUnion::asGuildMessageChannel))
-                .into(Type.of(format, NewsChannel.class), channel(GuildChannelUnion::asNewsChannel))
-                .into(Type.of(format, StageChannel.class), channel(GuildChannelUnion::asStageChannel))
-                .into(Type.of(format, TextChannel.class), channel(GuildChannelUnion::asTextChannel))
-                .into(Type.of(format, ThreadChannel.class), channel(GuildChannelUnion::asThreadChannel))
-                .into(Type.of(format, VoiceChannel.class), channel(GuildChannelUnion::asVoiceChannel));
+                .into(Type.of(GuildChannel.class), uni((source, _) -> lossless(source)))
+                .into(Type.of(AudioChannel.class), channel(GuildChannelUnion::asAudioChannel))
+                .into(Type.of(GuildMessageChannel.class), channel(GuildChannelUnion::asGuildMessageChannel))
+                .into(Type.of(NewsChannel.class), channel(GuildChannelUnion::asNewsChannel))
+                .into(Type.of(StageChannel.class), channel(GuildChannelUnion::asStageChannel))
+                .into(Type.of(TextChannel.class), channel(GuildChannelUnion::asTextChannel))
+                .into(Type.of(ThreadChannel.class), channel(GuildChannelUnion::asThreadChannel))
+                .into(Type.of(VoiceChannel.class), channel(GuildChannelUnion::asVoiceChannel));
 
         typeAdapters.forEach(((entry, adapter) ->
                 proteus.register((Type<Object>) entry.getKey(), (Type<Object>) entry.getValue(), (Mapper.UniMapper<Object, Object>) adapter))
@@ -90,33 +74,4 @@ public class TypeAdapters {
             }
         });
     }
-
-    /// Checks if a path for the given types exists.
-    ///
-    /// @param source the source type to check
-    /// @param target the target type to check
-    /// @return `true` if a type adapter exists
-    public boolean exists(@NotNull Type<?> source, @NotNull Type<?> target) {
-        return proteus.existsPath(source, target);
-    }
-
-    /// Retrieves a type adapter.
-    ///
-    /// @param type the type to get the adapter for
-    /// @return the type adapter or an empty Optional if none found
-    public Optional<TypeAdapter<?, ?>> get(@Nullable Class<?> type) {
-        return Optional.empty();
-    }
-
-    public record TypeFormat(OptionType type) implements Format {
-
-        @Override
-        public boolean equals(Format other) {
-            if (other instanceof TypeFormat(OptionType optionType)) {
-                return type.equals(optionType);
-            }
-            return false;
-        }
-    }
-
 }
--- core/src/main/java/com/github/kaktushose/jda/commands/dispatching/handling/command/SlashCommandHandler.java
+++ core/src/main/java/com/github/kaktushose/jda/commands/dispatching/handling/command/SlashCommandHandler.java
@@ -5,8 +5,6 @@ import com.github.kaktushose.jda.commands.definitions.interactions.command.Optio
 import com.github.kaktushose.jda.commands.definitions.interactions.command.SlashCommandDefinition;
 import com.github.kaktushose.jda.commands.dispatching.DispatchingContext;
 import com.github.kaktushose.jda.commands.dispatching.Runtime;
-import com.github.kaktushose.jda.commands.dispatching.adapter.internal.TypeAdapters;
-import com.github.kaktushose.jda.commands.dispatching.adapter.internal.TypeAdapters.TypeFormat;
 import com.github.kaktushose.jda.commands.dispatching.context.InvocationContext;
 import com.github.kaktushose.jda.commands.dispatching.events.interactions.CommandEvent;
 import com.github.kaktushose.jda.commands.dispatching.handling.EventHandler;
@@ -23,7 +21,6 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-import java.nio.channels.Channel;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -76,10 +73,9 @@ public final class SlashCommandHandler extends EventHandler<SlashCommandInteract
             OptionDataDefinition optionData = optionDataDefinitions.get(i);
             OptionMapping optionMapping = optionMappings.get(i);
             Type<?> sourceType = toType(optionMapping);
-            Type<?> targetType = Type.of(new TypeFormat(optionData.optionType()), optionData.type());
+            Type<?> targetType = Type.of(optionData.type());
 
             log.debug("Trying to adapt input '{}' as type '{}' to type '{}'", optionMapping, sourceType, targetType);
-            System.out.println(proteus.existsPath(sourceType, targetType));
             ConversionResult<?> result = proteus.convert(toValue(optionMapping), (Type<Object>) sourceType, (Type<Object>) targetType);
 
             switch (result) {
@@ -108,23 +104,22 @@ public final class SlashCommandHandler extends EventHandler<SlashCommandInteract
 
     private Type<?> toType(OptionMapping optionMapping) {
         OptionType type = optionMapping.getType();
-        TypeFormat format = new TypeFormat(type);
         return switch (type) {
-            case STRING -> Type.of(format, String.class);
-            case INTEGER -> Type.of(format, Long.class);
-            case BOOLEAN -> Type.of(format, Boolean.class);
+            case STRING -> Type.of(String.class);
+            case INTEGER -> Type.of(Long.class);
+            case BOOLEAN -> Type.of(Boolean.class);
             case USER -> {
                 Member member = optionMapping.getAsMember();
                 if (member == null) {
-                    yield Type.of(format, User.class);
+                    yield Type.of(User.class);
                 }
-                yield Type.of(format, Member.class);
+                yield Type.of(Member.class);
             }
-            case CHANNEL -> Type.of(format, GuildChannelUnion.class);
-            case ROLE -> Type.of(format, Role.class);
-            case MENTIONABLE -> Type.of(format, IMentionable.class);
-            case NUMBER -> Type.of(format, Double.class);
-            case ATTACHMENT -> Type.of(format, Message.Attachment.class);
+            case CHANNEL -> Type.of(GuildChannelUnion.class);
+            case ROLE -> Type.of(Role.class);
+            case MENTIONABLE -> Type.of(IMentionable.class);
+            case NUMBER -> Type.of(Double.class);
+            case ATTACHMENT -> Type.of(Message.Attachment.class);
             case UNKNOWN, SUB_COMMAND, SUB_COMMAND_GROUP -> throw new IllegalArgumentException(
                     "Invalid option type %s. Please report this error the the devs of jda-commands.".formatted(type)
             );
