024748a14b0b6e68a2d2154343d4d6aeafa92ba9
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2025-06-15 00:19:06 +0200

    add support for discord locale and command options

--- core/src/test/java/MockTest.java
+++ core/src/test/java/MockTest.java
@@ -3,6 +3,8 @@ import com.github.kaktushose.jda.commands.annotations.interactions.Interaction;
 import com.github.kaktushose.jda.commands.annotations.interactions.Param;
 import com.github.kaktushose.jda.commands.dispatching.events.interactions.CommandEvent;
 import internal.TestScenario;
+import internal.invocation.SlashCommandInvocation;
+import internal.invocation.SlashCommandInvocation.Option;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,7 +22,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MockTest {
 
     private static final String COMMAND_NAME = "test";
-    private static final String REPLY = "Hello World";
+    private static final String REPLY = "Hello";
+    private static final String INPUT = "World";
     private TestScenario scenario;
 
     @BeforeEach
@@ -42,8 +46,11 @@ class MockTest {
         assertEquals(expected.getDescription(), actual.getDescription());
         assertEquals(expected.isRequired(), actual.isRequired());
 
-        CompletableFuture<MessageCreateData> reply = scenario.slash(COMMAND_NAME).invoke();
-        assertEquals("%s %s".formatted(REPLY, null), reply.join().getContent());
+        CompletableFuture<MessageCreateData> reply = scenario.slash(COMMAND_NAME)
+                .option("message", Option.string(INPUT))
+                .invoke();
+
+        assertEquals("%s %s".formatted(REPLY, INPUT), reply.join().getContent());
     }
 
     @Interaction
--- core/src/test/java/internal/invocation/SlashCommandInvocation.java
+++ core/src/test/java/internal/invocation/SlashCommandInvocation.java
@@ -1,21 +1,23 @@
 package internal.invocation;
 
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
 import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.IEventManager;
+import net.dv8tion.jda.api.interactions.DiscordLocale;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.requests.restaction.WebhookMessageEditAction;
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import net.dv8tion.jda.api.utils.messages.MessageEditData;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
@@ -24,8 +26,8 @@ public class SlashCommandInvocation {
 
     private final IEventManager eventManager;
     private final SlashCommandInteractionEvent event;
-    private final InteractionHook hook;
     private final CompletableFuture<MessageCreateData> reply = new CompletableFuture<>();
+    private final Map<String, OptionMapping> optionMappings = new HashMap<>();
     private User user;
 
     public SlashCommandInvocation(IEventManager eventManager, String command) {
@@ -33,13 +35,13 @@ public class SlashCommandInvocation {
 
         event = mock(SlashCommandInteractionEvent.class);
         when(event.getFullCommandName()).thenReturn(command);
-        when(event.getOption(anyString())).thenCallRealMethod();
+        when(event.getOption(anyString())).then(invocation -> optionMappings.get((String) invocation.getArgument(0)));
         when(event.deferReply(anyBoolean())).thenReturn(mock(ReplyCallbackAction.class));
 
         user = mock(User.class);
         when(event.getUser()).then((_) -> user);
 
-        hook = mock(InteractionHook.class);
+        InteractionHook hook = mock(InteractionHook.class);
         when(event.getHook()).thenReturn(hook);
         lenient().when(hook.sendMessage(any(MessageCreateData.class))).then(invocation -> {
             reply.complete(MessageCreateData.fromEditData(invocation.getArgument(0)));
@@ -49,15 +51,28 @@ public class SlashCommandInvocation {
             reply.complete(MessageCreateData.fromEditData(invocation.getArgument(0)));
             return mock(WebhookMessageEditAction.class);
         });
+
+        lenient().when(event.getUserLocale()).thenReturn(DiscordLocale.ENGLISH_US);
+        lenient().when(event.getGuildLocale()).thenReturn(DiscordLocale.ENGLISH_US);
     }
 
-    public SlashCommandInvocation options(Collection<OptionMapping> optionMapping) {
-        when(event.getOptions()).thenReturn(List.copyOf(optionMapping));
+    public SlashCommandInvocation option(String name, OptionMapping mapping) {
+        optionMappings.put(name, mapping);
         return this;
     }
 
     public SlashCommandInvocation channel(MessageChannelUnion channel) {
         when(event.getChannel()).thenReturn(channel);
+        return this;
+    }
+
+    public SlashCommandInvocation guildLocale(DiscordLocale locale) {
+        when(event.getGuildLocale()).thenReturn(locale);
+        return this;
+    }
+
+    public SlashCommandInvocation userLocale(DiscordLocale locale) {
+        when(event.getUserLocale()).thenReturn(locale);
         return this;
     }
 
@@ -77,4 +92,49 @@ public class SlashCommandInvocation {
         return reply;
     }
 
+    public static class Option {
+
+        public static OptionMapping string(String value) {
+            return mapping(OptionType.STRING, OptionMapping::getAsString, value);
+        }
+
+        public static OptionMapping integer(Integer value) {
+            return mapping(OptionType.INTEGER, OptionMapping::getAsInt, value);
+        }
+
+        public static OptionMapping bool(Boolean value) {
+            return mapping(OptionType.BOOLEAN, OptionMapping::getAsBoolean, value);
+        }
+
+        public static OptionMapping user(User value) {
+            return mapping(OptionType.USER, OptionMapping::getAsUser, value);
+        }
+
+        public static OptionMapping channel(GuildChannelUnion value) {
+            return mapping(OptionType.CHANNEL, OptionMapping::getAsChannel, value);
+        }
+
+        public static OptionMapping role(Role value) {
+            return mapping(OptionType.ROLE, OptionMapping::getAsRole, value);
+        }
+
+        public static OptionMapping mentionable(IMentionable value) {
+            return mapping(OptionType.MENTIONABLE, OptionMapping::getAsMentionable, value);
+        }
+
+        public static OptionMapping number(Double value) {
+            return mapping(OptionType.NUMBER, OptionMapping::getAsDouble, value);
+        }
+
+        public static OptionMapping attachment(Message.Attachment value) {
+            return mapping(OptionType.ATTACHMENT, OptionMapping::getAsAttachment, value);
+        }
+
+        private static <T> OptionMapping mapping(OptionType type, Function<OptionMapping, T> methodCall, T value) {
+            OptionMapping mapping = mock(OptionMapping.class);
+            when(mapping.getType()).thenReturn(type);
+            when(methodCall.apply(mapping)).thenReturn(value);
+            return mapping;
+        }
+    }
 }
