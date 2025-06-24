86168285fc022e933d62025976d04ed8ffd2499c
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2025-06-24 19:04:26 +0200

    use override conflict strategy for default mappers

--- core/src/main/java/com/github/kaktushose/jda/commands/dispatching/adapter/internal/TypeAdapters.java
+++ core/src/main/java/com/github/kaktushose/jda/commands/dispatching/adapter/internal/TypeAdapters.java
@@ -2,6 +2,7 @@ package com.github.kaktushose.jda.commands.dispatching.adapter.internal;
 
 import com.github.kaktushose.jda.commands.dispatching.adapter.TypeAdapter;
 import io.github.kaktushose.proteus.Proteus;
+import io.github.kaktushose.proteus.ProteusBuilder;
 import io.github.kaktushose.proteus.mapping.Mapper;
 import io.github.kaktushose.proteus.type.Type;
 import net.dv8tion.jda.api.entities.*;
@@ -17,6 +18,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Function;
 
+import static io.github.kaktushose.proteus.ProteusBuilder.ConflictStrategy.OVERRIDE;
 import static io.github.kaktushose.proteus.mapping.Mapper.uni;
 import static io.github.kaktushose.proteus.mapping.MappingResult.failure;
 import static io.github.kaktushose.proteus.mapping.MappingResult.lossless;
@@ -41,25 +43,25 @@ public class TypeAdapters {
     public TypeAdapters(@NotNull Map<Entry<Type<?>, Type<?>>, TypeAdapter<?, ?>> typeAdapters) {
         proteus = Proteus.global();
 
-        proteus.from(NUMBER).into(STRING, uni((source, _) -> lossless(String.valueOf(source))));
+        proteus.from(NUMBER).into(STRING, uni((source, _) -> lossless(String.valueOf(source))), OVERRIDE);
 
-        proteus.from(MEMBER).into(USER, uni((source, _) -> lossless(source.getUser())));
-        proteus.from(USER).into(MEMBER, uni((_, _ ) -> failure("A member is required, but only a user got provided")));
+        proteus.from(MEMBER).into(USER, uni((source, _) -> lossless(source.getUser())), OVERRIDE);
+        proteus.from(USER).into(MEMBER, uni((_, _ ) -> failure("A member is required, but only a user got provided")), OVERRIDE);
 
         proteus.into(MENTIONABLE)
-                .from(USER, uni((source, _) -> lossless(source)))
-                .from(MEMBER, uni((source, _) -> lossless(source)))
-                .from(ROLE, uni((source, _) -> lossless(source)));
+                .from(USER, uni((source, _) -> lossless(source)), OVERRIDE)
+                .from(MEMBER, uni((source, _) -> lossless(source)), OVERRIDE)
+                .from(ROLE, uni((source, _) -> lossless(source)), OVERRIDE);
 
         proteus.from(CHANNEL)
-                .into(Type.of(GuildChannel.class), uni((source, _) -> lossless(source)))
-                .into(Type.of(AudioChannel.class), channel(GuildChannelUnion::asAudioChannel))
-                .into(Type.of(GuildMessageChannel.class), channel(GuildChannelUnion::asGuildMessageChannel))
-                .into(Type.of(NewsChannel.class), channel(GuildChannelUnion::asNewsChannel))
-                .into(Type.of(StageChannel.class), channel(GuildChannelUnion::asStageChannel))
-                .into(Type.of(TextChannel.class), channel(GuildChannelUnion::asTextChannel))
-                .into(Type.of(ThreadChannel.class), channel(GuildChannelUnion::asThreadChannel))
-                .into(Type.of(VoiceChannel.class), channel(GuildChannelUnion::asVoiceChannel));
+                .into(Type.of(GuildChannel.class), uni((source, _) -> lossless(source)), OVERRIDE)
+                .into(Type.of(AudioChannel.class), channel(GuildChannelUnion::asAudioChannel), OVERRIDE)
+                .into(Type.of(GuildMessageChannel.class), channel(GuildChannelUnion::asGuildMessageChannel), OVERRIDE)
+                .into(Type.of(NewsChannel.class), channel(GuildChannelUnion::asNewsChannel), OVERRIDE)
+                .into(Type.of(StageChannel.class), channel(GuildChannelUnion::asStageChannel), OVERRIDE)
+                .into(Type.of(TextChannel.class), channel(GuildChannelUnion::asTextChannel), OVERRIDE)
+                .into(Type.of(ThreadChannel.class), channel(GuildChannelUnion::asThreadChannel), OVERRIDE)
+                .into(Type.of(VoiceChannel.class), channel(GuildChannelUnion::asVoiceChannel), OVERRIDE);
 
         typeAdapters.forEach(((entry, adapter) ->
                 proteus.register((Type<Object>) entry.getKey(), (Type<Object>) entry.getValue(), (Mapper.UniMapper<Object, Object>) adapter))
