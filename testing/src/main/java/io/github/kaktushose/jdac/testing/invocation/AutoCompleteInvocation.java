49cc0d0aa152bbe9f5785df634cf730267c3ec3a
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2025-06-24 19:22:46 +0200

    fix autocomplete

--- testing/src/main/java/io/github/kaktushose/jdac/testing/invocation/AutoCompleteInvocation.java
+++ testing/src/main/java/io/github/kaktushose/jdac/testing/invocation/AutoCompleteInvocation.java
@@ -1,12 +1,14 @@
 package io.github.kaktushose.jdac.testing.invocation;
 
-import com.github.kaktushose.jda.commands.dispatching.events.interactions.AutoCompleteEvent;
 import io.github.kaktushose.jdac.testing.TestScenario.Context;
 import io.github.kaktushose.jdac.testing.invocation.internal.Invocation;
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
+import net.dv8tion.jda.api.interactions.AutoCompleteQuery;
 import net.dv8tion.jda.api.interactions.InteractionType;
 import net.dv8tion.jda.api.interactions.commands.Command.Choice;
 import net.dv8tion.jda.api.interactions.commands.CommandAutoCompleteInteraction;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.requests.restaction.interactions.AutoCompleteCallbackAction;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -17,25 +19,30 @@ import java.util.concurrent.TimeoutException;
 import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.Mockito.*;
 
-public final class AutoCompleteInvocation extends Invocation<AutoCompleteEvent, List<Choice>> {
+public final class AutoCompleteInvocation extends Invocation<CommandAutoCompleteInteractionEvent, List<Choice>> {
 
     private final CompletableFuture<List<Choice>> result;
+    private final AutoCompleteQuery focusedOption;
     private String value;
 
     public AutoCompleteInvocation(Context context, String command, String option) {
-        super(context, AutoCompleteEvent.class, InteractionType.COMMAND_AUTOCOMPLETE);
+        super(context, CommandAutoCompleteInteractionEvent.class, InteractionType.COMMAND_AUTOCOMPLETE);
 
         value = "";
-        when(event.getValue()).thenReturn(value);
+
+        focusedOption = mock(AutoCompleteQuery.class);
+        lenient().when(event.getFocusedOption()).thenReturn(focusedOption);
+        lenient().when(focusedOption.getName()).thenReturn(option);
+        lenient().when(focusedOption.getValue()).thenReturn(value);
 
         CommandAutoCompleteInteraction interaction = mock(CommandAutoCompleteInteraction.class);
+        when(event.getInteraction()).thenReturn(interaction);
         when(interaction.getFullCommandName()).thenReturn(command);
-        when(event.getName()).thenReturn(option);
 
         result = new CompletableFuture<>();
         doAnswer(invocation -> {
             result.complete(invocation.getArgument(0));
-            return null;
+            return mock(AutoCompleteCallbackAction.class);
         }).when(event).replyChoices(anyCollection());
     }
 
@@ -49,7 +56,7 @@ public final class AutoCompleteInvocation extends Invocation<AutoCompleteEvent, 
     }
 
     public AutoCompleteInvocation optionType(OptionType type) {
-        when(event.getOptionType()).thenReturn(type);
+        when(focusedOption.getType()).thenReturn(type);
         return this;
     }
 
