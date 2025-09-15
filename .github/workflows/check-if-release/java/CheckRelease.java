6fa7c1be8c867232bec7803eab58234921ed8b4e
Goldmensch <pm@goldmensch.dev>
2025-09-15 14:11:36 +0200

    Restart refactor check-if-release

---
+++ .github/workflows/check-if-release/java/CheckRelease.java
@@ -0,0 +1,13 @@
+//JAVA 24
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+final static String REGEX = "^Release (0|[1-9]\\d*)(?:[.](0|[1-9]\\d*))?(?:[.](0|[1-9]\\d*))?(?:-(0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(?:\\+([0-9a-zA-Z-]+(?:[.][0-9a-zA-Z-]+)*))? *(?:: *(.+))?$";
+
+void main() {
+    String msg = System.getenv("MSG");
+    Matcher matcher = Pattern.compile(REGEX).matcher(msg);
+
+    System.out.println(System.getenv("GITHUB_OUTPUT"));
+}
