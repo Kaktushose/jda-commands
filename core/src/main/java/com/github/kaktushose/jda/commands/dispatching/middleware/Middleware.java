622cae9528a614b9f51dd51065c789dadc092626
Goldmensch <pm@goldmensch.dev>
2025-09-21 17:52:26 +0200

    Add support for middlewares bound to certain controllers

--- core/src/main/java/com/github/kaktushose/jda/commands/dispatching/middleware/Middleware.java
+++ core/src/main/java/com/github/kaktushose/jda/commands/dispatching/middleware/Middleware.java
@@ -1,19 +1,23 @@
 package com.github.kaktushose.jda.commands.dispatching.middleware;
 
 
-import com.github.kaktushose.jda.commands.JDACBuilder;
 import com.github.kaktushose.jda.commands.dispatching.context.InvocationContext;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
+import org.jspecify.annotations.Nullable;
 
+import java.util.Collection;
 import java.util.function.Consumer;
 
 /// Middlewares run just before an interaction event gets dispatched. They are used to perform additional checks or add
-/// more info the [InvocationContext]. Middlewares can have different [Priorities][Priority] dictating their priority
+/// more info the [com.github.kaktushose.jda.commands.dispatching.context.InvocationContext]. Middlewares can have different [Priorities][com.github.kaktushose.jda.commands.dispatching.middleware.Priority] dictating their priority
 /// on execution.
 ///
-/// Register them at the [JDACBuilder#middleware(Priority, Middleware)]  or use the
+/// Register them at the [com.github.kaktushose.jda.commands.JDACBuilder#middleware(Priority, Middleware)]  or use the
 /// [`@Implementation.Validator`]({@docRoot}/io.github.kaktushose.jda.commands.extension.guice/com/github/kaktushose/jda/commands/guice/Implementation.Middleware.html)
 /// annotation of the guice extension.
+///
+/// If you want a [Middleware] to only run for certain interaction controllers, just implement [#runFor()]
+/// returning the classes of the interaction controllers for which the middleware should run.
 ///
 /// ### Example
 /// ```java
@@ -27,6 +31,24 @@ import java.util.function.Consumer;
 ///     }
 /// }
 /// ```
+///
+/// ### Example (run only for specific interaction controller)
+/// ```java
+/// @Middleware(priority = Priority.NORMAL)
+/// public class CustomMiddleware implements Middleware {
+///     private static final Logger log = LoggerFactory.getLogger(FirstMiddleware.class);
+///
+///     @Override
+///     public void accept(InvocationContext<?> context) {
+///         log.info("run custom middleware");
+///     }
+///
+///     @Override
+///     public Collection<Class<?>> runFor() {
+///         return List.of(HelloController.class);
+///     }
+/// }
+/// ```
 @FunctionalInterface
 public interface Middleware extends Consumer<InvocationContext<?>> {
 
@@ -35,4 +57,13 @@ public interface Middleware extends Consumer<InvocationContext<?>> {
     /// @param context the [InvocationContext] of the current interaction event
     void accept(InvocationContext<?> context);
 
+    /// Declares the interaction controllers for which this middleware should run.
+    ///
+    /// If this method returns `null`, then this [Middleware] runs for all interaction controllers.
+    ///
+    /// @return the classes of the interaction controllers or null (run for all interaction controllers)
+    @Nullable
+    default Collection<Class<?>> runFor() {
+        return null;
+    }
 }
