775fb8115601f2006cf90fe36dc8e0ccc3f54695
Goldmensch <pm@goldmensch.dev>
2025-10-06 21:34:41 +0200

    Log currently parsed wiki file

---
+++ wiki/hooks/on_page_logging.py
@@ -0,0 +1,7 @@
+# hooks/log_current_file.py
+import logging
+
+def on_page_markdown(markdown, page, config, files):
+    log = logging.getLogger("mkdocs")
+    log.info(f"Parsing: {page.file.src_path}")
+    return markdown
