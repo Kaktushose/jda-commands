6f4db8775388fb8fb499f9f3e7de0b22a7ba393d
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2025-11-13 20:02:35 +0100

    readd constraint middleware

--- core/src/main/java/io/github/kaktushose/jdac/dispatching/middleware/internal/Middlewares.java
+++ core/src/main/java/io/github/kaktushose/jdac/dispatching/middleware/internal/Middlewares.java
@@ -2,6 +2,7 @@ package io.github.kaktushose.jdac.dispatching.middleware.internal;
 
 import io.github.kaktushose.jdac.dispatching.middleware.Middleware;
 import io.github.kaktushose.jdac.dispatching.middleware.Priority;
+import io.github.kaktushose.jdac.dispatching.middleware.impl.ConstraintMiddleware;
 import io.github.kaktushose.jdac.dispatching.middleware.impl.PermissionsMiddleware;
 import io.github.kaktushose.jdac.embeds.error.ErrorMessageFactory;
 import io.github.kaktushose.jdac.permissions.PermissionsProvider;
@@ -17,6 +18,7 @@ public class Middlewares {
     public Middlewares(Collection<Map.Entry<Priority, Middleware>> userDefined, ErrorMessageFactory errorMessageFactory, PermissionsProvider permissionsProvider) {
         SortedMap<Priority, Set<Middleware>> middlewareMap = new TreeMap<>(Map.of(
                 Priority.PERMISSIONS, new HashSet<>(List.of(new PermissionsMiddleware(permissionsProvider, errorMessageFactory))),
+                Priority.NORMAL, new HashSet<>(List.of(new ConstraintMiddleware(errorMessageFactory))),
                 Priority.HIGH, new HashSet<>(),
                 Priority.LOW, new HashSet<>()
         ));
