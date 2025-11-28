39e99d8ca8f6a9080f3c73fc1d3c44cc9aec19e8
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2025-11-28 17:20:14 +0100

    add javadocs for cv2 methods

--- core/src/main/java/io/github/kaktushose/jdac/dispatching/events/ReplyableEvent.java
+++ core/src/main/java/io/github/kaktushose/jdac/dispatching/events/ReplyableEvent.java
@@ -20,6 +20,7 @@ import io.github.kaktushose.jdac.embeds.internal.Embeds;
 import io.github.kaktushose.jdac.message.i18n.I18n;
 import io.github.kaktushose.jdac.message.placeholder.Entry;
 import net.dv8tion.jda.api.components.ActionComponent;
+import net.dv8tion.jda.api.components.Component;
 import net.dv8tion.jda.api.components.MessageTopLevelComponent;
 import net.dv8tion.jda.api.components.buttons.Button;
 import net.dv8tion.jda.api.components.selections.SelectMenu;
@@ -195,6 +196,23 @@ public sealed abstract class ReplyableEvent<T extends GenericInteractionCreateEv
         return new ConfigurableReply(getReplyConfig());
     }
 
+    /// Acknowledgement of this event with V2 Components.
+    ///
+    /// Using V2 components removes the top-level component limit,
+    /// and allows more components in total ({@value Message#MAX_COMPONENT_COUNT_IN_COMPONENT_TREE}).
+    ///
+    /// They also allow you to use a larger choice of components, such as any component extending [MessageTopLevelComponent],
+    /// as long as they are [compatible][Component.Type#isMessageCompatible()].
+    ///
+    /// The character limit for the messages also gets changed to {@value Message#MAX_CONTENT_LENGTH_COMPONENT_V2}.
+    ///
+    /// This, however, comes with a few drawbacks:
+    ///
+    ///   - You cannot send content, embeds, polls or stickers
+    ///   - It does not support voice messages
+    ///   - It does not support previewing files
+    ///   - URLs don't create embeds
+    ///   - You cannot switch this message back to not using Components V2 (you can however upgrade a message to V2)
     public Message reply(MessageTopLevelComponent component, MessageTopLevelComponent... components) {
         return new ComponentReplyAction(getReplyConfig(), component, components).reply();
     }
