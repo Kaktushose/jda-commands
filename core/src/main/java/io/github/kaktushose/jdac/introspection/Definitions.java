2f6dcd09b6a64343012a086ecfd958a3ad0da119
Goldmensch <pm@goldmensch.dev>
2025-12-25 23:22:04 +0100

    Add IntrospectionAccess where useful

--- core/src/main/java/io/github/kaktushose/jdac/introspection/Definitions.java
+++ core/src/main/java/io/github/kaktushose/jdac/introspection/Definitions.java
@@ -1,11 +1,12 @@
 package io.github.kaktushose.jdac.introspection;
 
 import io.github.kaktushose.jdac.definitions.Definition;
+import io.github.kaktushose.jdac.definitions.interactions.InteractionRegistry;
 
 import java.util.Collection;
 import java.util.function.Predicate;
 
-public interface Definitions {
+public sealed interface Definitions permits InteractionRegistry {
     Collection<Definition> all();
     <T extends Definition> Collection<T> find(Class<T> type, Predicate<T> predicate);
     <T extends Definition> T findFirst(Class<T> type, Predicate<T> predicate);
