a5eac6742d36759e47c90478cc6dcab896355dff
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2026-01-02 19:01:40 +0100

    first pass on modal mapping

--- core/src/main/java/io/github/kaktushose/jdac/dispatching/handling/ModalHandler.java
+++ core/src/main/java/io/github/kaktushose/jdac/dispatching/handling/ModalHandler.java
@@ -2,12 +2,16 @@ package io.github.kaktushose.jdac.dispatching.handling;
 
 import io.github.kaktushose.jdac.definitions.interactions.CustomId;
 import io.github.kaktushose.jdac.definitions.interactions.ModalDefinition;
+import io.github.kaktushose.jdac.definitions.interactions.ModalDefinition.MappedParameter;
 import io.github.kaktushose.jdac.dispatching.Runtime;
 import io.github.kaktushose.jdac.dispatching.events.interactions.ModalEvent;
 import io.github.kaktushose.jdac.introspection.internal.IntrospectionImpl;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.interactions.modals.ModalMapping;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @ApiStatus.Internal
@@ -23,6 +27,27 @@ public final class ModalHandler extends EventHandler<ModalInteractionEvent> {
                 it.definitionId().equals(CustomId.fromMerged(event.getModalId()).definitionId())
         );
 
-        return new PreparationResult(modal, List.of(new ModalEvent()));
+        List<@Nullable Object> arguments = new ArrayList<>();
+        arguments.add(new ModalEvent());
+        for (MappedParameter parameter : modal.parameters()) {
+            ModalMapping value = event.getValue(parameter.id());
+            if (value == null) {
+                try {
+                    value = event.getValueByUniqueId(Integer.parseInt(parameter.id()));
+                } catch (NumberFormatException _) {}
+            }
+            if (value == null) {
+                arguments.add(null);
+            } else {
+                Object argument = switch (parameter.mapping()) {
+                    case STRING -> value.getAsString();
+                    case MENTIONS -> value.getAsMentions();
+                    case ATTACHMENT_LIST -> value.getAsAttachmentList();
+                };
+                arguments.add(argument);
+            }
+        }
+
+        return new PreparationResult(modal, arguments);
     }
 }
