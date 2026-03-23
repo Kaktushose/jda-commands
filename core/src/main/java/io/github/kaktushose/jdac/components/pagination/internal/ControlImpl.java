db205a1c5016dd5ca9cfe42601505befe32dc21b
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2026-03-23 12:19:40 +0100

    simplify control

--- core/src/main/java/io/github/kaktushose/jdac/components/pagination/internal/ControlImpl.java
+++ core/src/main/java/io/github/kaktushose/jdac/components/pagination/internal/ControlImpl.java
@@ -1,16 +1,16 @@
 package io.github.kaktushose.jdac.components.pagination.internal;
 
 import io.github.kaktushose.jdac.components.pagination.layout.Control;
-import net.dv8tion.jda.api.components.actionrow.ActionRowChildComponentUnion;
+import net.dv8tion.jda.api.components.actionrow.ActionRowChildComponent;
 
-public record ControlImpl(ControlType type, ActionRowChildComponentUnion component, int threshold) implements Control {
+public record ControlImpl(ActionRowChildComponent component, int threshold) implements Control {
 
-    public ControlImpl(ControlType type, ActionRowChildComponentUnion component) {
-        this(type, component, 1);
+    public ControlImpl(ActionRowChildComponent component) {
+        this(component, 1);
     }
 
     @Override
     public Control threshold(int threshold) {
-        return new ControlImpl(type, component, threshold);
+        return new ControlImpl(component, threshold);
     }
 }
--- core/src/main/java/io/github/kaktushose/jdac/components/pagination/layout/Control.java
+++ core/src/main/java/io/github/kaktushose/jdac/components/pagination/layout/Control.java
@@ -1,30 +1,12 @@
 package io.github.kaktushose.jdac.components.pagination.layout;
 
 import io.github.kaktushose.jdac.components.pagination.internal.ControlImpl;
-import io.github.kaktushose.jdac.dispatching.reply.dynamic.ButtonComponent;
-import io.github.kaktushose.jdac.dispatching.reply.dynamic.menu.StringSelectComponent;
 import net.dv8tion.jda.api.components.actionrow.ActionRowChildComponent;
 
 public interface Control extends Threshold {
 
-    static Control start(ButtonComponent control) {
-        return new ControlImpl(ControlType.START, control);
-    }
-
-    static Control back(ButtonComponent control) {
-        return new ControlImpl(ControlType.BACK, control);
-    }
-
-    static Control forth(ButtonComponent control) {
-        return new ControlImpl(ControlType.FORTH, control);
-    }
-
-    static Control end(ButtonComponent control) {
-        return new ControlImpl(ControlType.END, control);
-    }
-
-    static Control pageSelect(StringSelectComponent control) {
-        return new ControlImpl(ControlType.PAGE_SELECT, control);
+    static Control of(ActionRowChildComponent component) {
+        return new ControlImpl(component);
     }
 
     @Override
@@ -32,11 +14,4 @@ public interface Control extends Threshold {
 
     ActionRowChildComponent component();
 
-    enum ControlType {
-        START,
-        BACK,
-        FORTH,
-        END,
-        PAGE_SELECT
-    }
 }
