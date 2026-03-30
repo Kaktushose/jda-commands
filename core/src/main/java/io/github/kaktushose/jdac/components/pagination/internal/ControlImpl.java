1305efdb3b4bc277e04ed748e19b9b5297fff7e8
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2026-03-30 23:31:43 +0200

    add direction and amount to controls

--- core/src/main/java/io/github/kaktushose/jdac/components/pagination/internal/ControlImpl.java
+++ core/src/main/java/io/github/kaktushose/jdac/components/pagination/internal/ControlImpl.java
@@ -3,14 +3,33 @@ package io.github.kaktushose.jdac.components.pagination.internal;
 import io.github.kaktushose.jdac.components.pagination.layout.Control;
 import net.dv8tion.jda.api.components.actionrow.ActionRowChildComponent;
 
-public record ControlImpl(ActionRowChildComponent component, int threshold) implements Control {
+public record ControlImpl(
+        ActionRowChildComponent component,
+        Direction direction,
+        int amount,
+        int threshold
+) implements Control {
 
-    public ControlImpl(ActionRowChildComponent component) {
-        this(component, 1);
+    public ControlImpl(ActionRowChildComponent component, Direction direction) {
+        this(component, direction, 1, 1);
+    }
+
+    public ControlImpl(ActionRowChildComponent component, Direction direction, int amount) {
+        this(component, direction, amount, 1);
     }
 
     @Override
     public Control threshold(int threshold) {
-        return new ControlImpl(component, threshold);
+        return new ControlImpl(component, direction, amount, threshold);
+    }
+
+    @Override
+    public Control direction(Direction direction) {
+        return new ControlImpl(component, direction, amount, threshold);
+    }
+
+    @Override
+    public Control amount(int amount) {
+        return new ControlImpl(component, direction, amount, threshold);
     }
 }
--- core/src/main/java/io/github/kaktushose/jdac/components/pagination/layout/Control.java
+++ core/src/main/java/io/github/kaktushose/jdac/components/pagination/layout/Control.java
@@ -5,13 +5,38 @@ import net.dv8tion.jda.api.components.actionrow.ActionRowChildComponent;
 
 public interface Control extends Threshold {
 
+    static Control forward(ActionRowChildComponent component) {
+        return new ControlImpl(component, Direction.FORWARD);
+    }
+
+    static Control forward(ActionRowChildComponent component, int amount) {
+        return new ControlImpl(component, Direction.FORWARD, amount);
+    }
+
+    static Control backward(ActionRowChildComponent component) {
+        return new ControlImpl(component, Direction.BACKWARD);
+    }
+
+    static Control backward(ActionRowChildComponent component, int amount) {
+        return new ControlImpl(component, Direction.BACKWARD, amount);
+    }
+
     static Control of(ActionRowChildComponent component) {
-        return new ControlImpl(component);
+        return new ControlImpl(component, Direction.NEUTRAL);
     }
 
     @Override
     Control threshold(int threshold);
 
+    Control direction(Direction direction);
+
+    Control amount(int amount);
+
     ActionRowChildComponent component();
 
+    enum Direction {
+        FORWARD,
+        NEUTRAL,
+        BACKWARD
+    }
 }
