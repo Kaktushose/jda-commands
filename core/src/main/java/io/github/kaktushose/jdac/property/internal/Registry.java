3ffa593869a6179ee80f62f6eb9084e260da840c
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2026-04-15 17:24:09 +0200

    fix Registry

--- core/src/main/java/io/github/kaktushose/jdac/property/internal/Registry.java
+++ core/src/main/java/io/github/kaktushose/jdac/property/internal/Registry.java
@@ -2,7 +2,6 @@ package io.github.kaktushose.jdac.property.internal;
 
 import io.github.kaktushose.jdac.property.JDACScope;
 import io.github.kaktushose.jdac.property.events.*;
-import dev.goldmensch.propane.event.Event;
 
 import java.util.Map;
 
@@ -10,16 +9,15 @@ import static java.util.Map.entry;
 
 public final class Registry extends dev.goldmensch.propane.Registry<JDACScope> {
     public static final Registry INSTANCE = new Registry();
-    private static final Map<Class<? extends Event<JDACScope>>, JDACScope> eventScopes = Map.ofEntries(
-            entry(FrameworkShutdownEvent.class, JDACScope.INITIALIZED),
-            entry(FrameworkStartEvent.class, JDACScope.INITIALIZED),
-            entry(InteractionFinishedEvent.class, JDACScope.INTERACTION),
-            entry(InteractionStartEvent.class, JDACScope.INTERACTION),
-            entry(RuntimeCloseEvent.class, JDACScope.RUNTIME),
-            entry(RuntimeOpenEvent.class, JDACScope.RUNTIME)
-    );
 
     Registry() {
-        super(eventScopes);
+        super(Map.ofEntries(
+                entry(FrameworkShutdownEvent.class, JDACScope.INITIALIZED),
+                entry(FrameworkStartEvent.class, JDACScope.INITIALIZED),
+                entry(InteractionFinishedEvent.class, JDACScope.INTERACTION),
+                entry(InteractionStartEvent.class, JDACScope.INTERACTION),
+                entry(RuntimeCloseEvent.class, JDACScope.RUNTIME),
+                entry(RuntimeOpenEvent.class, JDACScope.RUNTIME)
+        ));
     }
 }
