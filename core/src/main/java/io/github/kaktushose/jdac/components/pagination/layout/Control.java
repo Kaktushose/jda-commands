cc9aa6c482a11b0efff07ded19973c0f1096afd8
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2026-04-18 15:05:21 +0200

    require concrete types in Control factory methods

--- core/src/main/java/io/github/kaktushose/jdac/components/pagination/layout/Control.java
+++ core/src/main/java/io/github/kaktushose/jdac/components/pagination/layout/Control.java
@@ -3,34 +3,36 @@ package io.github.kaktushose.jdac.components.pagination.layout;
 import io.github.kaktushose.jdac.components.pagination.internal.PageButtonImpl;
 import io.github.kaktushose.jdac.components.pagination.internal.PageSelectImpl;
 import net.dv8tion.jda.api.components.actionrow.ActionRowChildComponent;
+import net.dv8tion.jda.api.components.buttons.Button;
+import net.dv8tion.jda.api.components.selections.StringSelectMenu;
 
 public sealed interface Control extends Threshold permits PageButton, PageSelect {
 
-    static PageButton forward(ActionRowChildComponent component) {
+    static PageButton forward(Button component) {
         return new PageButtonImpl(component, Direction.FORWARD);
     }
 
-    static PageButton forward(ActionRowChildComponent component, int amount) {
+    static PageButton forward(Button component, int amount) {
         return new PageButtonImpl(component, Direction.FORWARD, amount);
     }
 
-    static PageButton backward(ActionRowChildComponent component) {
+    static PageButton backward(Button component) {
         return new PageButtonImpl(component, Direction.BACKWARD);
     }
 
-    static PageButton backward(ActionRowChildComponent component, int amount) {
+    static PageButton backward(Button component, int amount) {
         return new PageButtonImpl(component, Direction.BACKWARD, amount);
     }
 
-    static PageButton neutral(ActionRowChildComponent component) {
+    static PageButton neutral(Button component) {
         return new PageButtonImpl(component, Direction.NEUTRAL);
     }
 
-    static PageSelect select(ActionRowChildComponent component) {
+    static PageSelect select(StringSelectMenu component) {
         return new PageSelectImpl(component, "Page %d");
     }
 
-    static PageSelect select(ActionRowChildComponent component, String format) {
+    static PageSelect select(StringSelectMenu component, String format) {
         return new PageSelectImpl(component, format);
     }
 
