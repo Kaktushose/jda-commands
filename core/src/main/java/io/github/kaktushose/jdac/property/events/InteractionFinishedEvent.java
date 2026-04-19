eddfa616213c56d571e3a610a2cd3ae633181369
Goldmensch <pm@goldmensch.dev>
2026-04-19 06:14:57 +0200

    Publish InteractionFinishedEvent

--- core/src/main/java/io/github/kaktushose/jdac/property/events/InteractionFinishedEvent.java
+++ core/src/main/java/io/github/kaktushose/jdac/property/events/InteractionFinishedEvent.java
@@ -6,15 +6,17 @@ import io.github.kaktushose.jdac.dispatching.context.InvocationContext;
 import io.github.kaktushose.jdac.property.JDACScope;
 import org.jspecify.annotations.Nullable;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.SequencedCollection;
 
 /// Published after an interaction controller method is called.
 ///
 /// @param invocationContext the [InvocationContext] that was used to invoke the interaction controller method
-/// @param exception         the [Exception] thrown by [Invoker#invoke(Object, SequencedCollection)] or `null` if no exception was thrown
+/// @param exception         the [Throwable] thrown by [Invoker#invoke(Object, SequencedCollection)] or `null` if no exception was thrown.
+///                          If an [InvocationTargetException] is thrown, the [cause][InvocationTargetException#getCause()] is unwrapped and passed instead.
 @IntrospectionAccess(JDACScope.INTERACTION)
 public record InteractionFinishedEvent(
         InvocationContext<?> invocationContext,
-        @Nullable Exception exception
+        @Nullable Throwable exception
 ) implements JDACEvent {
 }
